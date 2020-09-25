cb4c7a2728dc72252c9d8ccb0838230485f1e970
DESKTOP-50I5KN0\abcdh <a1043684532@gmail.com>
2020-09-25 19:32:00 +0800

    first

---
+++ MU/Assets/Scripts/StellarData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+[Serializable]
+public class StellarData 
+{
+    public string starName = "未知"; //名称
+    public string stellarClassification = "O"; //恒星类型
+    public int temperature = 52000; //温度(K)
+    public int age = 400; //年龄(万年)
+    public int metalAmount = 1; //原行星盘金属量(1,2,3) 1最多    
+}
